30b3269cfb528143c8c8e63414c3c179c94addf7
webcodeeducation <66624767+webcodeeducation@users.noreply.github.com>
2021-08-23 20:31:31 +0530

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,76 @@
+<!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Center Name</h3>
+            <p>
+              Address 1 <br>
+              Address<br>
+              Address 3 <br><br>
+              <strong>Phone:</strong> 123456789.<br>
+              <strong>Email:</strong> info@example.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="about.php">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="courses.php">Courses</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="trainers.php">Trainers</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="contact.php">Contact</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="terms.php">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="policy.php">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Login Panel</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://emaxindia.in/exampanel/student/index.php">Student Login</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://emaxindia.in/exampanel/center/index.php">Center Login</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://emaxindia.in/exampanel/student/studentverification.php">Student Verification</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://emaxindia.in/exampanel/student/getcertificate.php">Student Certificate</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="https://emaxindia.in/exampanel/center/index.php">Center Login</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Join Our Newsletter</h4>
+            <p>Give us your email id and get latest news from our side.</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="mr-md-auto text-center text-md-left">
+        <div class="copyright">
+          &copy; Copyright <strong><span>Emax Education</span></strong>. All Rights Reserved <?php echo CENTERCODE; ?>
+        </div>
+        <div class="credits">
+          Designed by <a href="https://emaxindia.in/">Emax Education</a>
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="bx bx-up-arrow-alt"></i></a>
+  <div id="preloader"></div>

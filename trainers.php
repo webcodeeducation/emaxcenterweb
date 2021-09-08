c91909bfff7433e94f06b0fad74371d038710847
webcodeeducation <66624767+webcodeeducation@users.noreply.github.com>
2021-09-08 16:09:53 +0530

    Update trainers.php

--- trainers.php
+++ trainers.php
@@ -37,13 +37,6 @@ $data = mysqli_fetch_assoc($result);
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Mentor - v2.2.1
-  * Template URL: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>

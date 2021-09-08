ff18752719e4a54e97bb1f6a69d52fe27d413b02
webcodeeducation <66624767+webcodeeducation@users.noreply.github.com>
2021-09-08 14:51:50 +0530

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -6,13 +6,12 @@
         <div class="row">
 
           <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>Center Name</h3>
+            <h3><?=$wdata['center_name']?></h3>
             <p>
               Address 1 <br>
-              Address<br>
               Address 3 <br><br>
-              <strong>Phone:</strong> 123456789.<br>
-              <strong>Email:</strong> info@example.com<br>
+              <strong>Phone:</strong> <?=$wdata['phone']?><br>
+              <strong>Email:</strong> <?=$wdata['email']?><br>
             </p>
           </div>
 

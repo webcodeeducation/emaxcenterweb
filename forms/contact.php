5fc311d179ea655a59c202a269d89a2abe2560c6
webcodeeducation <66624767+webcodeeducation@users.noreply.github.com>
2021-09-09 17:05:04 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,53 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+include 'secret.php';
+include '../connection/config.php';
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+$sql = 'select * from center where c_id='.CENTERID;
+$result = mysqli_query($conn,$sql);
+$data = mysqli_fetch_assoc($result);
+$to = $data['email'];
+$from = $_POST['email'];
+$subject = $_POST['subject'];
+$fromName = $_POST['name'];
+$comments = $_POST['message'];
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+$subject = "Customer: Enquiry Form Mail"; 
+ 
+$htmlContent = ' 
+    <html> 
+    <head> 
+        <title>Welcome to Our Center</title> 
+    </head> 
+    <body> 
+        <h1>Thanks you for joining with us!</h1> 
+        <table cellspacing="0" style="border: 2px dashed #FB4314; width: 100%;"> 
+            <tr> 
+                <th>Name:</th><td>'.$fromName.'</td> 
+            </tr> 
+            <tr style="background-color: #e0e0e0;"> 
+                <th>Email:</th><td>'.$from.'</td> 
+            </tr> 
+            <tr> 
+                <th>Subject:</th><td>'.$subject.'</td> 
+            </tr>
+            <tr> 
+                <th>Comment:</th><td>'.$comments.'</td> 
+            </tr> 
+        </table> 
+    </body> 
+    </html>'; 
+ 
+// Set content-type header for sending HTML email 
+$headers = "MIME-Version: 1.0" . "\r\n"; 
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n"; 
+ 
+// Additional headers 
+$headers .= 'From: '.$fromName.'<'.$from.'>' . "\r\n"; 
+$headers .= 'Bcc: payment@emaxindia.in' . "\r\n"; 
+// Send email 
+if(mail($to, $subject, $htmlContent, $headers)){ 
+    echo 'Email has sent successfully.'; 
+}else{ 
+   echo 'Email sending failed.'; 
+}
 ?>

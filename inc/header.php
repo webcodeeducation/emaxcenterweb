ebef74c25f1f5854a82be458d22eba2020caf2c4
webcodeeducation <66624767+webcodeeducation@users.noreply.github.com>
2021-09-11 12:13:53 +0530

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -23,6 +23,7 @@
               <li><a href="https://emaxindia.in/exampanel/student/getcertificate.php" target="_blank">Student Certificate</a></li>
               <li><a href="https://emaxindia.in/exampanel/center/index.php" target="_blank">Center Login</a></li>
               <li><a href="https://emaxindia.in/exampanel/user-center/login.php" target="_blank">Teacher Login</a></li>
+              <li><a href="https://emaxindia.in/exampanel/center/center_certificate_verification.php" target="_blank">Top Center Verification</a></li>
               <li><a href="https://emaxindia.in/exampanel/student/confirmwebinar.php" target="_blank">Webinar Verify</a></li>
             </ul>
           </li>

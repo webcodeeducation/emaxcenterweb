300bb395327ccb6051ffeefcd68bc2de7abf8530
webcodeeducation <66624767+webcodeeducation@users.noreply.github.com>
2021-09-27 18:28:42 +0530

    Update footer.php

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,7 @@
+<a href="https://api.whatsapp.com/send?phone=<?=$data['whatsapp']?>&text=Hello%21%20Team%20m%C3%A1s%20" class="whatfloat" target="_blank">
+<i class="fa fa-whatsapp what-float"></i>
+</a>
+
 <!-- ======= Footer ======= -->
   <footer id="footer">
 
@@ -41,7 +45,7 @@
           <div class="col-lg-4 col-md-6 footer-newsletter">
             <h4>Join Our Newsletter</h4>
             <p>Give us your email id and get latest news from our side.</p>
-            <form action="" method="post">
+            <form action="forms/subscribe.php" method="post">
               <input type="email" name="email"><input type="submit" value="Subscribe">
             </form>
           </div>
@@ -61,11 +65,11 @@
         </div>
       </div>
       <div class="social-links text-center text-md-right pt-3 pt-md-0">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        <a href="<?=$data['twitter']?>" class="twitter" target="_blank"><i class="bx bxl-twitter"></i></a>
+        <a href="<?=$data['facebook']?>" class="facebook" target="_blank"><i class="bx bxl-facebook"></i></a>
+        <a href="<?=$data['instagram']?>" class="instagram" target="_blank"><i class="bx bxl-instagram"></i></a>
+        <a href="<?=$data['google']?>" class="google-plus" target="_blank"><i class="bx bxl-skype"></i></a>
+        <a href="<?=$data['linkedin']?>" class="linkedin" target="_blank"><i class="bx bxl-linkedin"></i></a>
       </div>
     </div>
   </footer><!-- End Footer -->

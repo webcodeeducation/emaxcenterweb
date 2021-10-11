502870317bbc651896d22edb764a04cb015b2bbe
webcodeeducation <66624767+webcodeeducation@users.noreply.github.com>
2021-10-11 10:06:21 +0530

    Update events.php

--- events.php
+++ events.php
@@ -108,7 +108,15 @@ $code = $cdata['branchcode'];
 
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-88401913-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
+  gtag('config', 'UA-88401913-1');
+</script>
 </body>
 </html>
 </php>

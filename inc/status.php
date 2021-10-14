c46da89f6392917a2a680744d7f306eb85abbece
webcodeeducation <66624767+webcodeeducation@users.noreply.github.com>
2021-10-14 10:34:23 +0530

    Add files via upload

---
+++ inc/status.php
@@ -0,0 +1,12 @@
+<?php
+include '../secret.php';
+include '../../connection/config.php';
+$test101 = 'select website_status from center where c_id='.CENTERID;
+$cres = mysqli_query($conn,$test101);
+$dd_web_data = mysqli_fetch_assoc($cres);
+$ws = $dd_web_data['website_status'];
+if($ws == 0){
+    header('Location: restriction.php');
+}
+
+?>

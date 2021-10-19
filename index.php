85c6d84b05735f6ce2781711f965f35c4f9bebd0
webcodeeducation <66624767+webcodeeducation@users.noreply.github.com>
2021-10-19 17:53:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 include 'secret.php';
 include '../connection/config.php';
+include 'inc/status.php';
 
 $sql = 'select * from center_websites_data where centerid='.CENTERID;
 $result = mysqli_query($conn,$sql);
+$mcount = mysqli_num_rows($result);
 $data = mysqli_fetch_assoc($result);
 $center_name = $data['center_name'];
 $address = $data['address'];
@@ -26,7 +28,7 @@ $code = $data7['branchcode'];
 <meta name="google-site-verification" content="ComQ2SdK2WR56w1Ik7Rg0EaLEzm2qPkMIlJb7hYso8c" />
   <title><?=$data['center_name']?></title>
   <meta name="robots" content="index, follow"/>
-        <meta name="keywords" content="Best Education Institute in <?=$data['center_name']?>"/>
+        <meta name="keywords" content="Best Education Institute  <?=$data['center_name']?>"/>
         <meta name="description" content="Top Institute in India <?=$data['center_name']?>"/>
 
   <!-- Favicons -->
@@ -66,11 +68,23 @@ $code = $data7['branchcode'];
       <?php 
       $ssql = 'select * from center_websites_sliders where isactive = 1 && cid="'.CENTERID.'"';
       $result = mysqli_query($conn, $ssql);
+      $count = mysqli_num_rows($result);
+      if($count > 0){
       while($row = mysqli_fetch_assoc($result)){?>
       <div class="slider-item" data-background-url="assets/images/<?php echo $row['slider'];?>">
         <h1 style="color:<?=$row['slider_data_color']?>"><?=$row['slider_data']?></h1>
       <h2 style="color:<?=$row['slider_data_2_color']?>"><?=$row['slider_data_2']?></h2>
       </div>
+      <?php } } else {?>
+      <div class="slider-item" data-background-url="assets/images/slider2.jpg">
+        <h1>Learning Today,<br>Leading Tomorrow</h1>
+      <h2>We are team of talented designers making websites with Bootstrap</h2>
+      </div>
+      <div class="slider-item" data-background-url="assets/images/slider3.jpg">
+        <h1>Learning Today,<br>Leading Tomorrow</h1>
+      <h2>We are team of talented designers making websites with Bootstrap</h2>
+      </div>
+      
       <?php } ?>
 
     </div>
@@ -95,12 +109,36 @@ $code = $data7['branchcode'];
 
         <div class="row">
           <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-            <img src="assets/images/<?=$data['home_about_photo']?>" class="img-fluid" alt="">
+              <?php if($mcount == 0){?>
+                  <img src="assets/img/about.jpg" class="img-fluid" alt="Emax India">
+                  ?>
+                  
+                  <?php
+              }else{ ?>
+              <img src="assets/images/<?=$data['home_about_photo']?>" class="img-fluid" alt="<?=$data['center_name']?>">
+              <?php
+              } ?>
+            
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
-            <!--h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3-->
             <p class="font-italic">
+                <?php if($mcount > 0){?>
               <?=$data['about_heading_txt']?>
+              <?php } else { ?>
+              <p class="font-italic">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua.
+            </p>
+            <ul>
+              <li><i class="icofont-check-circled"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+              <li><i class="icofont-check-circled"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+              <li><i class="icofont-check-circled"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+            </ul>
+            <p>
+              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+            </p>
+              
+              <?php } ?>
             </p>
            
             <a href="about.php" class="learn-more-btn">Learn More</a>
@@ -119,11 +157,33 @@ $code = $data7['branchcode'];
 <?php
 $sql = 'select * from center_website_dynamic_fields where isactive=1 && centerid='.CENTERID;
 $result = mysqli_query($conn,$sql);
+$fcount = mysqli_num_rows($result);
+if($fcount > 0){
 while($rowf=mysqli_fetch_assoc($result)){
 ?>
           <div class="col-lg-3 col-6 text-center">
             <span data-toggle="counter-up"><?=$rowf['field_value']?></span>
             <p><?=$rowf['field_name']?></p>
+          </div>
+<?php } } else{ ?>
+<div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">1232</span>
+            <p>Students</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">64</span>
+            <p>Courses</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">42</span>
+            <p>Events</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up">15</span>
+            <p>Trainers</p>
           </div>
 <?php } ?>
         </div>
@@ -138,9 +198,18 @@ while($rowf=mysqli_fetch_assoc($result)){
         <div class="row">
           <div class="col-lg-4 d-flex align-items-stretch">
             <div class="content">
+                <?php if($mcount > 0){ ?>
               <h3>Why Choose <?=$data['center_name']?>?</h3>
+              <?php } else { ?>
+              <h3>Why Choose Mentor?</h3>
+              <?php } ?>
               <p>
+                  <?php if($mcount > 0){ ?>
                 <?=$data['about_heading_txt']?>
+                <?php } else{ ?>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
+                Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus optio ad corporis.
+                <?php } ?>
               </p>
               <div class="text-center">
                 <a href="about.php" class="more-btn">Learn More <i class="bx bx-chevron-right"></i></a>
@@ -151,8 +220,10 @@ while($rowf=mysqli_fetch_assoc($result)){
             <div class="icon-boxes d-flex flex-column justify-content-center">
               <div class="row">
                 <?php
-                $hsql = 'SELECT * FROM center_websites_headings where cid='.CENTERID;
+                $hsql = 'SELECT * FROM center_websites_headings where isactive=1 && cid='.CENTERID;
                 $resulth = mysqli_query($conn, $hsql);
+                $hcount = mysqli_num_rows($resulth);
+                if($hcount > 0){
                 while($rowh=mysqli_fetch_assoc($resulth)){
                 ?>
                 
@@ -163,8 +234,29 @@ while($rowf=mysqli_fetch_assoc($result)){
                     <p><?=$rowh['heading_txt']?></p>
                   </div>
                 </div>
+                <?php } } else{ ?>
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <i class="bx bx-receipt"></i>
+                    <h4>Corporis voluptates sit</h4>
+                    <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
+                  </div>
+                </div>
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <i class="bx bx-cube-alt"></i>
+                    <h4>Ullamco laboris ladore pan</h4>
+                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+                  </div>
+                </div>
+                <div class="col-xl-4 d-flex align-items-stretch">
+                  <div class="icon-box mt-4 mt-xl-0">
+                    <i class="bx bx-images"></i>
+                    <h4>Labore consequatur</h4>
+                    <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
+                  </div>
+                </div>
                 <?php } ?>
-                
                 
               </div>
             </div><!-- End .content-->
@@ -184,11 +276,21 @@ while($rowf=mysqli_fetch_assoc($result)){
 		<div class="col-12 col-carousel">
 			<div class="owl-carousel carousel-main">
 			    <?php
+			    $scount = mysqli_num_rows($result_students);
+			    if($scount > 0){
 			    while($row=mysqli_fetch_assoc($result_students)){
 			    if($row['studentlogo'] != ''){
 			    ?>
-				<div><img src="../../exampanel/images/logo/studentlogo/<?=$row['studentlogo']?>" width="250px" height="250px"></div>
-				<?php } } ?>
+				<div><img src="../../exampanel/images/logo/studentlogo/<?=$row['studentlogo']?>" width="250px" height="250px">
+				</div>
+				<?php } } } else{ ?>
+				<div><img src="assets/students/photo_1.jpg?text=1"></div>
+				<div><img src="assets/students/photo_2.jpg?text=1"></div>
+				<div><img src="assets/students/photo_3.jpg?text=1"></div>
+				<div><img src="assets/students/photo_4.jpg?text=1"></div>
+				<div><img src="assets/students/photo_5.jpg?text=1"></div>
+				<div><img src="assets/students/photo_6.jpg?text=1"></div>
+				<?php } ?>
 			</div>
 		</div>
 	</div>
@@ -211,6 +313,8 @@ while($rowf=mysqli_fetch_assoc($result)){
     <?php
 $sql = 'select * from center_websites_courses where isactive=1 && cid='.CENTERID;
 $result = mysqli_query($conn,$sql);
+$ccount = mysqli_num_rows($result);
+if($ccount > 0){
 while($row=mysqli_fetch_assoc($result)){
 ?>
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
@@ -223,6 +327,63 @@ while($row=mysqli_fetch_assoc($result)){
 
                 <h3><a href="course-details.php?id=<?=$row['id']?>"><?=$row['title']?></a></h3>
                 <p><?=$row['detaiils']?></p>
+              </div>
+            </div>
+          </div> <!-- End Course Item-->
+          <?php } } else{ ?>
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="course-item">
+              <img src="assets/img/course-1.jpg" class="img-fluid" alt="...">
+              <div class="course-content">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                  <h4>Web Development</h4>
+                  <p class="price">$169</p>
+                </div>
+
+                <h3><a href="course-details.php">Website Design</a></h3>
+                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+                <div class="trainer d-flex justify-content-between align-items-center">
+                  
+                  
+                </div>
+              </div>
+            </div>
+          </div> <!-- End Course Item-->
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-md-0">
+            <div class="course-item">
+              <img src="assets/img/course-2.jpg" class="img-fluid" alt="...">
+              <div class="course-content">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                  <h4>Marketing</h4>
+                  <p class="price">$250</p>
+                </div>
+
+                <h3><a href="course-details.php">Search Engine Optimization</a></h3>
+                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+                <div class="trainer d-flex justify-content-between align-items-center">
+                  
+                  
+                </div>
+              </div>
+            </div>
+          </div> <!-- End Course Item-->
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0">
+            <div class="course-item">
+              <img src="assets/img/course-3.jpg" class="img-fluid" alt="...">
+              <div class="course-content">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                  <h4>Content</h4>
+                  <p class="price">$180</p>
+                </div>
+
+                <h3><a href="course-details.php">Copywriting</a></h3>
+                <p>Et architecto provident deleniti facere repellat nobis iste. Id facere quia quae dolores dolorem tempore.</p>
+                <div class="trainer d-flex justify-content-between align-items-center">
+                  
+                  
+                </div>
               </div>
             </div>
           </div> <!-- End Course Item-->
@@ -251,6 +412,8 @@ while($row=mysqli_fetch_assoc($result)){
           <?php
 $sql = 'select * from center_websites_trainers where isactive=1 && cid='.CENTERID;
 $result = mysqli_query($conn,$sql);
+$tcount = mysqli_num_rows($result);
+if($tcount > 0){
 while($row=mysqli_fetch_assoc($result)){
 ?>
           <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
@@ -261,6 +424,47 @@ while($row=mysqli_fetch_assoc($result)){
                 <span><?=$row['title']?></span>
                 <p>
                   <?=$row['course_name']?>
+                </p>
+              </div>
+            </div>
+          </div>
+          <?php } } else{ ?>
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+              <img src="assets/img/trainers/trainer-1.jpg" class="img-fluid" alt="">
+              <div class="member-content">
+                <h4>Walter White</h4>
+                <span>Web Development</span>
+                <p>
+                  Magni qui quod omnis unde et eos fuga et exercitationem. Odio veritatis perspiciatis quaerat qui aut aut aut
+                </p>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+              <img src="assets/img/trainers/trainer-2.jpg" class="img-fluid" alt="">
+              <div class="member-content">
+                <h4>Sarah Jhinson</h4>
+                <span>Marketing</span>
+                <p>
+                  Repellat fugiat adipisci nemo illum nesciunt voluptas repellendus. In architecto rerum rerum temporibus
+                </p>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch">
+            <div class="member">
+              <img src="assets/img/trainers/trainer-3.jpg" class="img-fluid" alt="">
+              <div class="member-content">
+                <h4>William Anderson</h4>
+                <span>Content</span>
+                <p>
+                  Voluptas necessitatibus occaecati quia. Earum totam consequuntur qui porro et laborum toro des clara
                 </p>
               </div>
             </div>
@@ -417,15 +621,7 @@ $('.carousel-main').owlCarousel({
 	navText: ['<span class="fas fa-chevron-left fa-2x"></span>','<span class="fas fa-chevron-right fa-2x"></span>'],
 });
   </script>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-88401913-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'UA-88401913-1');
-</script>
 </body>
 
-</html>
+</php>

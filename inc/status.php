dc3d52305443f21032cebb41b83e6e4d8a714161
webcodeeducation <66624767+webcodeeducation@users.noreply.github.com>
2021-11-28 15:16:22 +0530

    Update status.php

--- inc/status.php
+++ inc/status.php
@@ -1,6 +1,6 @@
 <?php
-include '../secret.php';
-include '../../connection/config.php';
+include 'secret.php';
+include '../connection/config.php';
 $test101 = 'select website_status from center where c_id='.CENTERID;
 $cres = mysqli_query($conn,$test101);
 $dd_web_data = mysqli_fetch_assoc($cres);

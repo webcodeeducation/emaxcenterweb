3d6a76bf3f818da92de3695205098c0845d4d705
webcodeeducation <66624767+webcodeeducation@users.noreply.github.com>
2021-11-30 17:05:14 +0530

    Update restriction.php

--- restriction.php
+++ restriction.php
@@ -11,6 +11,9 @@ $test101 = 'select website_status from center where c_id='.CENTERID;
 $cres = mysqli_query($conn,$test101);
 $dd_web_data = mysqli_fetch_assoc($cres);
 $ws = $dd_web_data['website_status'];
+if($ws['website_status'] == 1){
+    header('Location: index.php');
+}
 ?>
 <!Doctype html>
 <html>
